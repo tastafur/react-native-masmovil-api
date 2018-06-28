b413fa79a7feff4e5c4b43c6be39b7201b970ab3
tastafur <ruben.romero@guidesmiths.com>
2018-06-28 23:50:56 +0200

    add bodyr parser

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 
 const app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-app.post('/login', (req, res) => {
+app.post('/', (req, res) => {
   if(!req.body.user) {
     return res.status(422).json({errors: {user: "can't be blank"}});
   }
@@ -27,5 +29,5 @@ app.post('/login', (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(3000, () => console.log('React Native Masmovil api listening on port 3000!'));
 
